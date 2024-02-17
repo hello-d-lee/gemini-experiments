9b4e13f5f6158676e67ff88aaeb85a2b2bf74901
sam <samharrison@google.com>
2024-02-17 00:22:01 +0000

    test

--- another_app_example/main.py
+++ another_app_example/main.py
@@ -72,7 +72,9 @@ def upload_image():
         )
         print("generated model response") 
 
-        return jsonify(responses.text)
+        return jsonify({
+            "response": marko.convert(responses.text)  # Use aggregated text 
+        })
         
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
